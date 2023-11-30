930a11150c8d291e42e60103e3da4e7324a39b0b
Jag <jag3dagster@gmail.com>
2023-11-30 01:39:44 -0800

    Nav Updated, NavIcon added

--- web/src/layouts/components/Nav.tsx
+++ web/src/layouts/components/Nav.tsx
@@ -1,22 +1,41 @@
 import { Navbar, Stack } from "@mantine/core";
-import TooltipIcon from "./shared/TooltipIcon";
 import { AiFillHome } from "react-icons/ai";
+import { FaDoorOpen } from "react-icons/fa";
+import { IoCubeSharp } from "react-icons/io5";
+import NavIcon from "./NavIcon";
 
 const Nav: React.FC = () => {
   return (
     <Navbar
+      height={800}
       width={{ base: '12%' }}
       p='md'
       fixed={false}
-      sx = {{ height: '80%' }}
     >
       <Navbar.Section grow>
         <Stack justify='center' align='center' spacing={5}>
-          <TooltipIcon
-            label='General'
+          <NavIcon
+            label={'General'}
+            destination={'/'}
             Icon={AiFillHome}
             color={'violet.6'}
             onClick={() => console.log('go home')}
+          />
+
+          <NavIcon 
+            label={'Rooms'}
+            destination={'/rooms'}
+            Icon={IoCubeSharp}
+            color={'violet.6'}
+            onClick={() => console.log('go rooms')}
+          />
+
+          <NavIcon 
+            label={'Portals'}
+            destination={'/portals'}
+            Icon={FaDoorOpen}
+            color={'violet.6'}
+            onClick={() => console.log('go portals')}
           />
         </Stack>
       </Navbar.Section>
