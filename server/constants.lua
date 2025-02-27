dc15f822262bc6175a506dc67cee3dc8ba787d60
Richard <41370482+YaBoiManiac@users.noreply.github.com>
2025-02-27 17:24:16 +1100

    Fixed Incorrect Spelling of "resourcePath" to fix script.

--- server/constants.lua
+++ server/constants.lua
@@ -5,6 +5,6 @@ constants.resourcePath = GetResourcePath(cache.resource):gsub('//', '/')
 constants.savedMLODir = 'saved_mlos'
 constants.savedMLODirPath = ('%s/%s'):format(constants.resourcePath, constants.savedMLODir)
 constants.generatedFilesDir = 'generated_files'
-constants.generatedFilesDirPath = ('%s/%s'):format(constants.reosurcePath, constants.generatedFilesDir)
+constants.generatedFilesDirPath = ('%s/%s'):format(constants.resourcePath, constants.generatedFilesDir)
 
 return table.freeze(constants)
