72f414cea402a0b43508b4ca19280141a0fb00eb
Jag <77469789+jag3dagster@users.noreply.github.com>
2025-10-13 00:27:22 -0700

    More algorithm progress

--- client/new_classes/CAudioOcclusionPath.lua
+++ client/new_classes/CAudioOcclusionPath.lua
@@ -48,6 +48,30 @@ function CAudioOcclusionPath:addChild(originNode, destinationNode, distance, glo
 end
 
 ---@param fromNode CAudioOcclusionNode
+---@param childOrigin CAudioOcclusionNode
+---@param childDestination CAudioOcclusionNode
+---@param distanceMinusOne number
+---@param portal CInteriorPortal
+---@param extraCondition fun(portal: CInteriorPortal, childOrigin: CAudioOcclusionNode, childDestination: CAudioOcclusionNode): boolean
+function CAudioOcclusionPath:tryAddChildFromPortal(fromNode, childOrigin, childDestination, distanceMinusOne, portal, extraCondition)
+    if portal.isMirror then return end
+
+    local globalPortalIndex = nil
+
+    if portal.toRoomIndex == childOrigin.index then
+        globalPortalIndex = portal.globalPortalIndices[1]
+    elseif portal.fromRoomIndex == childOrigin.index then
+        globalPortalIndex = portal.globalPortalIndices[2]
+    end
+
+    if globalPortalIndex ~= nil
+        and (not extraCondition or extraCondition(portal, childOrigin, childDestination))
+    then
+        self:addChild(childOrigin, childDestination, distanceMinusOne, globalPortalIndex)
+    end
+end
+
+---@param fromNode CAudioOcclusionNode
 ---@param toNode CAudioOcclusionNode
 ---@return boolean
 function CAudioOcclusionPath:isRelevant(fromNode, toNode)
@@ -62,6 +86,61 @@ end
 local function findPathInList(pathList, fromNodeKey, toNodeKey, distance)
     local nodePairKey = ToUInt32(fromNodeKey - toNodeKey) + distance
     return pathList[nodePairKey]
+end
+
+---@param pathList table<number, CAudioOcclusionPath>
+---@param fromNodeKey number
+---@param toNodeKey number
+---@param distance number
+---@return boolean
+local function hasAlreadyBeenFound(pathList, fromNodeKey, toNodeKey, distance)
+    local nodePairKey = ToUInt32(fromNodeKey - toNodeKey)
+
+    for i = 1, distance do
+        if pathList[nodePairKey + i] then
+            return true
+        end
+    end
+
+    return false
+end
+
+---@param pathList table<number, CAudioOcclusionPath>
+---@param fromNode CAudioOcclusionNode
+---@param toNode CAudioOcclusionNode
+---@param distance number
+---@return CAudioOcclusionPath
+---@return boolean
+---@return number
+local function getOrCreatePath(pathList, fromNode, toNode, distance)
+    local key = 0
+
+    if fromNode.index ~= toNode.index then
+        key = ToUInt32(fromNode.key - toNode.key) + distance
+    end
+
+    local curPath = pathList[key]
+    local created = false
+
+    if not curPath then
+        curPath = CAudioOcclusionPath:new(fromNode, toNode, distance)
+        created = true
+    end
+
+    return curPath, created, key
+end
+
+---@param pathList table<number, CAudioOcclusionPath>
+---@param pathKeys table<number, number[]>
+---@param curPath CAudioOcclusionPath
+---@param created boolean
+---@param distance number
+---@param key number
+local function finalizePathInsertion(pathList, pathKeys, curPath, created, distance, key)
+    if created and curPath.childList > 0 then
+        pathList[key] = curPath
+        table.insert(pathKeys[distance], key)
+    end
 end
 
 ---@param pathList table<number, CAudioOcclusionPath>
@@ -109,7 +188,89 @@ local function generateRoutes(pathList, pathKeys, pair, distance)
                     table.insert(pathKeys[distance], curPath.uintKey)
                 end
             else
-                
+                for edgeIndex = 1, #edges do
+                    local edge = edges[edgeIndex]
+
+                    if path:isRelevant(edge, pair.toNode) then
+                        local curPath = findPathInList(pathList, pair.fromNode.key, pair.toNode.key, distance)
+
+                        local new = false
+                        if not curPath then
+                            curPath = CAudioOcclusionPath:new(pair.fromNode, pair.toNode, distance)
+                            new = true
+                        end
+
+                        for portalIndex = 1, pair.fromNode.activePortalCount do
+                            local portal = pair.fromNode.activePortals[portalIndex]
+
+                            if not portal.isMirror then
+                                local globalPortalIndex = nil
+
+                                if portal.toRoomIndex == edge.index then
+                                    globalPortalIndex = portal.globalPortalIndices[1]
+                                elseif portal.fromRoomIndex == edge.index then
+                                    globalPortalIndex = portal.globalPortalIndices[2]
+                                end
+
+                                if globalPortalIndex ~= nil
+                                    and (edge.index == portal.fromRoomIndex
+                                        or (portal.fromRoomIndex ~= 0 and portal.toRoomIndex ~= 0))
+                                then
+                                    curPath:addChild(edge, pair.toNode, distance -1, globalPortalIndex)
+                                end
+                            end
+                        end
+
+                        if new and curPath.childCount > 0 then
+                            pathList[curPath.uintKey] = curPath
+                            table.insert(pathKeys[distance], curPath.uintKey)
+                        end
+                    end
+                end
+            end
+        elseif distance > 3 then
+            for edgeIndex = 1, #edges do
+                local edge = edges[edgeIndex]
+
+                if path:isRelevant(edge, pair.toNode) then
+                    local alreadyFound = hasAlreadyBeenFound(pathList, pair.fromNode.key, pair.toNode.key, distance)
+
+                    if not alreadyFound then
+                        local curPath = findPathInList(pathList, pair.fromNode.key, pair.toNode.key, distance)
+
+                        local new = false
+                        if not curPath then
+                            curPath = CAudioOcclusionPath:new(pair.fromNode, pair.toNode, distance)
+                            new = true
+                        end
+
+                        for portalIndex = 1, pair.fromNode.activePortalCount do
+                            local portal = pair.fromNode.activePortals[portalIndex]
+
+                            if not portal.isMirror then
+                                local globalPortalIndex = nil
+
+                                if portal.toRoomIndex == edge.index then
+                                    globalPortalIndex = portal.globalPortalIndices[1]
+                                elseif portal.fromRoomIndex == edge.index then
+                                    globalPortalIndex = portal.globalPortalIndices[2]
+                                end
+
+                                if globalPortalIndex ~= nil
+                                    and (edge.index == pair.toNode.index
+                                        or (portal.fromRoomIndex ~= 0 and portal.toRoomIndex ~= 0))
+                                then
+                                    curPath:addChild(edge, pair.toNode, distance - 1, globalPortalIndex)
+                                end
+                            end
+                        end
+
+                        if new and curPath.childCount > 0 then
+                            pathList[curPath.uintKey] = curPath
+                            table.insert(pathKeys[distance], curPath.uintKey)
+                        end
+                    end
+                end
             end
         end
     end
